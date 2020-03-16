2eb5ba6f3f7cc0b38b740dffc645bc1870bf581f
Jaime <jaimel2011@gmail.com>
2020-03-16 12:31:27 -0500

    init

---
+++ src/stack.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { Appbar, Avatar, useTheme } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { BottomTabs } from './bottomTabs';
+import { Details } from './details';
+import { StackNavigatorParamlist } from './types';
+
+const Stack = createStackNavigator<StackNavigatorParamlist>();
+
+export const StackNavigator = () => {
+  const theme = useTheme();
+
+  return (
+    <Stack.Navigator
+      initialRouteName="FeedList"
+      headerMode="screen"
+      screenOptions={{
+        header: ({ scene, previous, navigation }) => {
+          const { options } = scene.descriptor;
+          const title =
+            options.headerTitle !== undefined
+              ? options.headerTitle
+              : options.title !== undefined
+              ? options.title
+              : scene.route.name;
+
+          return (
+            <Appbar.Header
+              theme={{ colors: { primary: theme.colors.surface } }}
+            >
+              {previous ? (
+                <Appbar.BackAction
+                  onPress={navigation.goBack}
+                  color={theme.colors.primary}
+                />
+              ) : (
+                <TouchableOpacity
+                  style={{ marginLeft: 10 }}
+                  onPress={() => {
+                    ((navigation as any) as DrawerNavigationProp<{}>).openDrawer();
+                  }}
+                >
+                  <Avatar.Icon size={40} icon="menu" />
+                </TouchableOpacity>
+              )}
+            </Appbar.Header>
+          );
+        },
+      }}
+    >
+      <Stack.Screen
+        name="FeedList"
+        component={BottomTabs}
+        options={({ route }) => {
+          console.log('!@# options', { route });
+          const routeName = route.state
+            ? route.state.routes[route.state.index].name
+            : 'Feed';
+          return { headerTitle: routeName };
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{ headerTitle: 'Tweet' }}
+      />
+    </Stack.Navigator>
+  );
+};
